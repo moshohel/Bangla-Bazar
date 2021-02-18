b19253ff9d858abd538f840bbde470abfb3d8fd0
Mohammad Shohel <mohammadshohel866@gmail.com>
2021-02-18 20:09:15 +0600

    Model Controllers Migration for Catagory, Brand, Admin & Database Schema

--- database/migrations/2021_02_17_131832_create_products_table.php
+++ database/migrations/2021_02_17_131832_create_products_table.php
@@ -14,9 +14,17 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->integer('brand_id')->unsigned();
             $table->string('title');
             $table->text('description');
+            $table->string('slug');
+            $table->integer('quantity')->default(1);
+            $table->integer('price');
+            $table->tinyInteger('status')->default(0);
+            $table->integer('offer_price')->nullable();
+            $table->integer('admin_id')->unsigned();;
             $table->timestamps();
         });
     }
